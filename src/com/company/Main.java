e8b61fd7b4ae619f09c3c83b478074d6c6070524
Ralph F <RaftsDev@gmail.com>
2020-10-01 23:57:26 -0400

    Feat: Initiallization of Map objects.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Map<String,String> languages = new HashMap<>();
+        languages.put("Java","High level, object oriented, independent");
+        languages.put("Python","interpreted, high-level, object oreinted");
+        languages.put("Algol","Algoritmic");
+        languages.put("BASIC","For beginners");
+
+        System.out.println(languages.get("BASIC"));
     }
 }
